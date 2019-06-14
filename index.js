fc33e9f6e64ded172ec75f26eb9f6f883bd7d0be
Jordan Mateen <jodanmateen1991@gmail.com>
2019-06-14 11:18:32 -0400

    adding comment branch

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// var dogButton = document.getElementById("dogButton");
-
-// dogButton.addEventListener("click", function(){
-//     this.innerHTML= "Generate Doggo";
-//     axios.get("https://dog.ceo/api/breeds/image/random").then(function(data){
-//         //console.log(data)
-//         var dogImageURL = data.data.message;
-//         document.getElementById("dogImage").src = dogImageURL
-//     });
-// });
-
-
-
-// var selectTag = document.getElementById('mySelect'); 
-// axios.get("https://dog.ceo/api/breeds/list/all").then(function(response){  
-//     var breeds = Object.keys(response.data.message); 
-//     for(var i = 0; i <= Object.keys(response.data.message).length; i++){ 
-//         var optionTag = document.createElement('option'); 
-//         optionTag.text = breeds[i]; 
-//         optionTag.value= breeds[i]; 
-//         selectTag.appendChild(optionTag); 
-//     }
-// });
-
-
-// selectTag.addEventListener('change',function(){  
-//     axios.get(`https://dog.ceo/api/breed/${this.value}/images/random`).then(function(data){ 
-//     var dogImageURL = data.data.message; 
-//     document.getElementById("dogImage").src = dogImageURL 
-//     });
-// });
-
+/**
+ * Code for retrieving dog data using vanilla javascript and axios checkout the solution branches
+ */
