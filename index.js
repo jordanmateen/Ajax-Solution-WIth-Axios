bf349d159789e3ecd258669441a5b78c2cfbb64f
Jordan Mateen <jodanmateen1991@gmail.com>
2019-06-14 11:11:46 -0400

    committing solution 1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-//Retrieving dag data using Vanilla JS and Axios
+var dogButton = document.getElementById("dogButton");
+
+dogButton.addEventListener("click", function(){
+    this.innerHTML= "Generate Doggo";
+    axios.get("https://dog.ceo/api/breeds/image/random").then(function(data){
+        //console.log(data)
+        var dogImageURL = data.data.message;
+        document.getElementById("dogImage").src = dogImageURL
+    });
+});
+
+
 
